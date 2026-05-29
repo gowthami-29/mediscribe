16f3ced3a4f64f413b13c87a6a25e5cf9b0419f4
gowthami-29 <gowthamikanchi29@gmail.com>
2026-05-29 13:36:28 +0530

    Added Electron desktop application support
    :wq

---
+++ frontend/electron/main.cjs
@@ -0,0 +1,14 @@
+const { app, BrowserWindow } = require('electron');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1400,
+    height: 900,
+    autoHideMenuBar: true,
+    title: 'ArogyaScribe'
+  });
+
+  win.loadURL('https://mediscribe-kohl.vercel.app');
+}
+
+app.whenReady().then(createWindow);
